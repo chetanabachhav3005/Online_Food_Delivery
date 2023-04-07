ae765341c8e8325f36412642ca8a901710866301
chetana <chetanabhaskarbachhav3005@gmail.com>
2023-04-07 20:38:39 +0530

    Restaurant Interface Methods Declared

--- src/main/java/com/hdfc/midterm/foodapp/service/IRestaurantsService.java
+++ src/main/java/com/hdfc/midterm/foodapp/service/IRestaurantsService.java
@@ -1,5 +1,26 @@
 package com.hdfc.midterm.foodapp.service;
 
+import java.util.List;
+
+import com.hdfc.midterm.foodapp.entity.Restaurants;
+import com.hdfc.midterm.foodapp.exception.RestaurantException;
+
 public interface IRestaurantsService {
-//
+  
+	
+	public Restaurants addRestaurant(Restaurants restaurant) throws RestaurantException;
+	
+	public Restaurants updateRestaurant(Restaurants restaurant) throws RestaurantException;
+	
+	public Restaurants removeRestaurant(Long restaurantId) throws RestaurantException;
+	
+	public Restaurants viewRestaurant(Long restaurantId) throws RestaurantException;
+	
+	
+	public List<Restaurants> findByRestaurantName(String restaurantName) throws RestaurantException;
+	
+	public List<Restaurants> findByCuisineType(String cuisineType) throws RestaurantException;
+	
+	public List<Restaurants> findByLocation(String Location) throws RestaurantException;
+
 }
