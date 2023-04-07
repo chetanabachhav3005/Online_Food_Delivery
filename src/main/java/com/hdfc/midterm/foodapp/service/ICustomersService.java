65ab7d851f325b2dfc1fc68ceb0e72d936aa0482
chetana <chetanabhaskarbachhav3005@gmail.com>
2023-04-07 20:16:24 +0530

    Customer Interface Methods Declared

--- src/main/java/com/hdfc/midterm/foodapp/service/ICustomersService.java
+++ src/main/java/com/hdfc/midterm/foodapp/service/ICustomersService.java
@@ -1,5 +1,15 @@
 package com.hdfc.midterm.foodapp.service;
 
+import com.hdfc.midterm.foodapp.entity.Customers;
+import com.hdfc.midterm.foodapp.exception.CustomerException;
+
 public interface ICustomersService {
-//
+	public Customers addCustomer(Customers customer) throws CustomerException;
+
+	public Customers updateCustomer(Customers customer) throws CustomerException;
+
+	public Customers removeCustomerById(Long customerId) throws CustomerException;
+
+	public Customers viewCustomer(Long customerId) throws CustomerException;
+
 }
