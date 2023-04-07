10c84b92b91eb5e4cfeaad13a3e980ee39ef71b6
chetana <chetanabhaskarbachhav3005@gmail.com>
2023-04-07 20:36:48 +0530

    Order Interface Methods Declared

--- src/main/java/com/hdfc/midterm/foodapp/service/IOrdersService.java
+++ src/main/java/com/hdfc/midterm/foodapp/service/IOrdersService.java
@@ -1,5 +1,17 @@
 package com.hdfc.midterm.foodapp.service;
 
+import com.hdfc.midterm.foodapp.entity.Orders;
+import com.hdfc.midterm.foodapp.exception.OrderException;
+
 public interface IOrdersService {
-//
+
+public Orders addOrder(Orders order)throws OrderException;
+	
+	public Orders updateOrder(Orders order)throws OrderException;
+	
+	public Orders removeOrder(Long orderId)throws OrderException;
+	
+	public Orders viewOrder(Long orderId)throws OrderException;
+	
+	
 }
