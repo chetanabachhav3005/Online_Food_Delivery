4b44a6b33b77a632a02a9e393ac86d4fe28766c2
chetana <chetanabhaskarbachhav3005@gmail.com>
2023-04-07 20:46:29 +0530

    Restaurant Repository Interface

--- src/main/java/com/hdfc/midterm/foodapp/repository/RestaurantsRepository.java
+++ src/main/java/com/hdfc/midterm/foodapp/repository/RestaurantsRepository.java
@@ -1,5 +1,7 @@
 package com.hdfc.midterm.foodapp.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,10 @@ import com.hdfc.midterm.foodapp.entity.Restaurants;
 
 @Repository
 public interface RestaurantsRepository extends JpaRepository<Restaurants, Long>{
-//
+
+	public List<Restaurants> findByrestaurantName(String restaurantName);
+	
+	public List<Restaurants> findBycuisineType(String cuisineType);
+	
+	public List<Restaurants> findByLocation(String Location);
 }
