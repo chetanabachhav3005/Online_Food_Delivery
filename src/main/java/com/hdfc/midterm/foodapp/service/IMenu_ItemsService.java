b922f4e537b8a72352df8ee303f301ecde9b9725
chetana <chetanabhaskarbachhav3005@gmail.com>
2023-04-07 20:36:05 +0530

    Item Interface Methods Declared

--- src/main/java/com/hdfc/midterm/foodapp/service/IMenu_ItemsService.java
+++ src/main/java/com/hdfc/midterm/foodapp/service/IMenu_ItemsService.java
@@ -1,5 +1,21 @@
 package com.hdfc.midterm.foodapp.service;
 
+import java.util.List;
+
+import com.hdfc.midterm.foodapp.entity.Menu_Items;
+import com.hdfc.midterm.foodapp.exception.ItemException;
+
+
+
 public interface IMenu_ItemsService {
-//
+	public Menu_Items addItem(Menu_Items item) throws ItemException;
+
+	public Menu_Items updateItem(Menu_Items item) throws ItemException;
+
+	public Menu_Items viewItem(Long menuItemId) throws ItemException;
+
+	public Menu_Items removeItem(Long menuItemId) throws ItemException;
+
+	public List<Menu_Items> viewAllItems() throws ItemException;
+
 }
